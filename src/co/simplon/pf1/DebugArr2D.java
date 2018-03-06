56c5c33b945dd633d2a31b31dd86e117fb2c528d
Utilisateur <Utilisateur@BNPPF006>
2018-03-06 17:21:22 +0100

    sav

--- src/co/simplon/pf1/DebugArr2D.java
+++ src/co/simplon/pf1/DebugArr2D.java
@@ -1,5 +1,7 @@
 package co.simplon.pf1;
 
+import java.util.Arrays;
+
 public class DebugArr2D {
 
 	public static char[] initializedArray(char c, int nb){
@@ -9,14 +11,27 @@ public class DebugArr2D {
 		}
 		return res;
 	}
+
 	public static char[][] initializedArray2D(char[] arr, int nb){
 		char[][] res= new char[nb][];
 		for(int i=0; i != res.length; ++i){
-			res[i]= arr;
+			res[i]= Arrays.copyOf(arr, arr.length);
 		}
 		return res;
 	}
-
+	
+	/*
+	public static char[][] initializedArray2D(char c, int nbLn, int nbCl){
+		char[][] res= new char[nbLn][nbCl];
+		for(int i=0; i < nbLn; ++i){
+			for (int j=0; j < nbCl; ++j) {
+				res[i][j]= c;
+			}
+		}
+		return res;
+	}
+	*/
+	
 	public static void display(char[][] arr2D){
 		for(char[] row : arr2D){
 			for(char c : row){
@@ -25,12 +40,23 @@ public class DebugArr2D {
 			System.out.println();
 		}
 	}
+	
 	public static void main(String[] args){
+		
 		char[][] screen= initializedArray2D(initializedArray(' ', 20), 20);
+		// char[][] screen= initializedArray2D(' ', 20, 20);
+
+		/*
+		for (int i=0; i < screen.length; ++i) {
+			System.out.println(screen[i] + "\n");
+		}
+		*/
+		
 		for(int i= 0; i != Math.min(screen.length, screen[0].length); ++i){
 			screen[i][i]='X';
 			screen[screen.length-i-1][i]='X';
 		}
+		
 		display(screen);
 	}
 
