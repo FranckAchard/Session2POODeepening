2f6d010e57e63689030e225f792a9a61a2e8ec3e
Utilisateur <Utilisateur@BNPPF006>
2018-03-07 11:10:05 +0100

    sav

--- src/co/simplon/pf1/DebugArray2DObjects.java
+++ src/co/simplon/pf1/DebugArray2DObjects.java
@@ -2,19 +2,35 @@ package co.simplon.pf1;
 
 import java.util.Arrays;
 
+import javax.sound.midi.Synthesizer;
+
 public class DebugArray2DObjects {
 
 	public static Stone[] initializedArray(Stone s, int nb){
 		Stone[] res= new Stone[nb];
 		for(int i=0; i != res.length; ++i){
-			res[i]= s;
+			// res[i]= s makes a shallow copy, thus each res[i] points to the same Stone object => instead we use a copy constructor 
+			//res[i]= s;
+			res[i]= new Stone(s);
 		}
 		return res;
 	}
 	public static Stone[][] initializedArray2D(Stone[] arr, int nb){
 		Stone[][] res= new Stone[nb][];
-		for(int i=0; i != res.length; ++i){
-			res[i]= Arrays.copyOf(arr, arr.length);
+		for(int i=0; i != res.length; ++i) {
+			// res[i]= arr makes a shallow copy, thus each res[i] points on the same array => instead we use a copy constructor for each Stone res[i][j]
+			//res[i]= arr;
+			// init res[i] (to avoid NullPointerException)
+			res[i]= new Stone[arr.length];
+			for (int j=0; j < arr.length; ++j) {
+				/*
+				System.out.println("i = " + i + " ; j = " + j);
+				System.out.println("arr[" + j + "] = " + arr[j]);
+				System.out.println("res[" + i + "][" + j + "] = " + res[i][j]);
+				*/
+				res[i][j]= new Stone(arr[j]);
+			}
+			
 		}
 		return res;
 	}
@@ -30,14 +46,31 @@ public class DebugArray2DObjects {
 	public static void main(String[] args){
 		Stone[][] screen= initializedArray2D(initializedArray(new Stone(false), 20), 20);
 
+		/*
+		for (int i=0; i < screen.length; ++i) {
+			System.out.println("screen[" + i + "] = " + screen[i]);
+		}
+		
+		for (int i=0; i < screen.length; ++i) {
+			System.out.println("screen[" + i + "] content :");
+			for (int j=0; j < screen[i].length; ++j) {
+				System.out.print("screen[" + i + "][" + j + "] = " + screen[i][j] + " ; ");
+			}
+			System.out.println();
+		}
+		*/
+		
+		/*
 		for (Stone[] s : screen) {
 			System.out.println(s);
 		}
-		
+		*/
+			
 		for(int i= 0; i != Math.min(screen.length, screen[0].length); ++i){
 			screen[i][i].setFirstPlayer(true);
 			screen[screen.length-i-1][i].setFirstPlayer(true);
 		}
+		
 		display(screen);
 	}
 }
--- src/co/simplon/pf1/Stone.java
+++ src/co/simplon/pf1/Stone.java
@@ -7,13 +7,20 @@ public class Stone {
 	public Stone(boolean firstPlayer) {
 		this.firstPlayer= firstPlayer;
 	}
+	
+	public Stone(Stone pStone) {
+		this.firstPlayer= pStone.firstPlayer;
+	}
+	
 	public boolean isFirstPlayer() {
 		return firstPlayer;
 	}
+	
 	public void setFirstPlayer(boolean firstPlayer) {
 		this.firstPlayer= firstPlayer;
 	}
+	
 	public String toString() {
-		return firstPlayer ? "O":"X";
+		return firstPlayer ? "O ":"X ";
 	}
 }
