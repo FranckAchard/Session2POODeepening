ad2101da4a9b6b353cfb86b1a8148c8968e74fe8
Utilisateur <Utilisateur@BNPPF006>
2018-03-12 11:34:43 +0100

    sav

--- src/consolidation/ReplaceInt.java
+++ src/consolidation/ReplaceInt.java
@@ -10,17 +10,39 @@ public class ReplaceInt {
 		}
 	}
 
+	public static int[] replaceNegativeNew(int[] pArray) {
+		int[] res= {};
+		for (int i=0; i < pArray.length; ++i) {
+			if (pArray[i] < 0) {
+				res[i]= 0;
+			} else {
+				res[i]= pArray[i];
+			}
+		}
+		return res;
+	}
+
+	public static void printIntArray(int[] pArray) {
+		for (int i=0; i < pArray.length; ++i) {
+			System.out.print(pArray[i] + " ; ");
+		}
+	}
+	
 	public static void main(String[] args) {
 
 		int[] array= {1, -5, 3, -2, 7};
-		for (int i=0; i < array.length; ++i) {
-			System.out.print(array[i] + " ; ");
-		}
+		int[] array2= {1, -5, 3, -2, 7};
+		int[] arrayNew= {};
+		printIntArray(array);
 		System.out.println();
 		replaceNegative(array);
-		for (int i=0; i < array.length; ++i) {
-			System.out.print(array[i] + " ; ");
-		}
+		printIntArray(array);
+
+		arrayNew= replaceNegativeNew(array2);
+		printIntArray(array2);
+		printIntArray(arrayNew);
+
+
 	}
 
 }
